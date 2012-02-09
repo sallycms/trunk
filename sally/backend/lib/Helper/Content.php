f54f1ea3bbdc4f95a9d1e735193f3ceba0ca3892
Zozi <zozi@webvariants.de>
2012-02-09 01:53:16 +0100

    use sly_Form for slice add/edit Forms

--- sally/backend/lib/Helper/Content.php
+++ sally/backend/lib/Helper/Content.php
@@ -23,38 +23,26 @@ class sly_Helper_Content {
 		else {
 			try {
 				ob_start();
-				?>
-				<div class="sly-form sly-slice-form" id="addslice">
-					<form action="index.php#slice<?php echo $position ?>" id="slice<?php echo $position ?>" method="post" enctype="multipart/form-data">
-						<div>
-							<input type="hidden" name="page" value="content" />
-							<input type="hidden" name="func" value="addArticleSlice" />
-							<input type="hidden" name="article_id" value="<?php echo $articleId ?>" />
-							<input type="hidden" name="clang" value="<?php echo $clang ?>" />
-							<input type="hidden" name="slot" value="<?php echo $slot ?>" />
-							<input type="hidden" name="module" value="<?php echo sly_html($module) ?>" />
-							<input type="hidden" name="pos" value="<?php echo $position ?>" />
-						</div>
-						<fieldset class="sly-form-col-1">
-							<legend><?php echo t('add_slice') ?>: <?php echo sly_translate($moduleService->getTitle($module), true) ?></legend>
-							<div class="sly-form-wrapper">
-								<div class="sly-contentpage-slice-input">
-									<?php
-									$renderer = new sly_Slice_Renderer($module);
-									print $renderer->renderInput('slicevalue');
-									?>
-								</div>
-								<div class="sly-form-row">
-									<div class="sly-form-buttonbar">
-										<input class="sly-form-submit" type="submit" name="btn_save" value="<?php echo t('add_slice') ?>" />
-									</div>
-								</div>
-							</div>
-						</fieldset>
-					</form>
-				</div>
+				$moduleTitle = $moduleService->getTitle($module);
+				$form = new sly_Form('index.php#slice'.$position, 'post', t('add_slice').': '.sly_translate($moduleTitle, true), '', 'slice'.$position);
+				$form->addHiddenValue('page', 'content');
+				$form->addHiddenValue('func', 'addArticleSlice');
+				$form->addHiddenValue('article_id', $articleId);
+				$form->addHiddenValue('clang', $clang);
+				$form->addHiddenValue('slot', $slot);
+				$form->addHiddenValue('module', sly_html($module));
+				$form->addHiddenValue('pos', $position);
+				$form->setSubmitButton(new sly_Form_Input_Button('submit', 'btn_save', t('add_slice')));
 
-				<?php
+				$renderer   = new sly_Slice_Renderer($module);
+				$sliceinput = new sly_Form_Fragment();
+				$sliceinput->setContent('<div class="sly-contentpage-slice-input">'.$renderer->renderInput('slicevalue').'</div>');
+
+				$form->add($sliceinput);
+				$form->addClass('sly-slice-form');
+
+				echo $form->render();
+
 				self::focusFirstElement();
 
 				sly_Core::dispatcher()->notify('SLY_SLICE_POSTVIEW_ADD', array(), array(
@@ -77,42 +65,40 @@ class sly_Helper_Content {
 
 	public static function printEditSliceForm(sly_Model_ArticleSlice $articleSlice, $values = array()) {
 		$moduleService = sly_Service_Factory::getModuleService();
-		$moduleTitle   = $moduleService->getTitle($articleSlice->getModule());
+		$module        = $articleSlice->getModule();
+		$moduleTitle   = $moduleService->getTitle($module);
 
 		try {
 			ob_start();
-			?>
-			<div class="sly-form sly-slice-form" id="editslice">
-				<form action="index.php#slice<?php echo $articleSlice->getPosition() ?>" method="post" enctype="multipart/form-data">
-					<div>
-						<input type="hidden" name="page" value="content" />
-						<input type="hidden" name="func" value="editArticleSlice" />
-						<input type="hidden" name="article_id" value="<?php echo $articleSlice->getArticleId() ?>" />
-						<input type="hidden" name="clang" value="<?php echo $articleSlice->getClang() ?>" />
-						<input type="hidden" name="slice_id" value="<?php echo $articleSlice->getId() ?>" />
-						<input type="hidden" name="slot" value="<?php echo $articleSlice->getSlot() ?>" />
-						<input type="hidden" name="pos" value="<?php echo $articleSlice->getPosition() ?>" />
-					</div>
-					<fieldset class="sly-form-col-1">
-						<legend><?php echo t('edit_slice') ?>: <?php echo sly_translate($moduleTitle, true) ?></legend>
-						<div class="sly-form-wrapper">
-							<div class="sly-contentpage-slice-input">
-								<?php
-									$renderer = new sly_Slice_Renderer($articleSlice->getModule(), $values);
-									print $renderer->renderInput('slicevalue');
-								?>
-							</div>
-							<div class="sly-form-row">
-								<div class="sly-form-buttonbar">
-									<input class="sly-form-submit" type="submit" value="<?php echo t('save') ?>" name="btn_save" />
-									<input class="sly-form-submit" type="submit" value="<?php echo t('apply') ?>" name="btn_update" />
-								</div>
-							</div>
-						</div>
-					</fieldset>
-				</form>
-			</div>
-			<?php
+			$form = new sly_Form('index.php#slice'.$articleSlice->getPosition(), 'post', t('edit_slice').': '.sly_translate($moduleTitle, true));
+			$form->addHiddenValue('page', 'content');
+			$form->addHiddenValue('func', 'editArticleSlice');
+			$form->addHiddenValue('article_id', $articleSlice->getArticleId());
+			$form->addHiddenValue('clang', $articleSlice->getClang());
+			$form->addHiddenValue('slice_id', $articleSlice->getId());
+			$form->addHiddenValue('slot', $articleSlice->getSlot());
+			$form->setSubmitButton(null);
+			$form->setResetButton(null);
+
+
+
+			$renderer   = new sly_Slice_Renderer($module, $values);
+			$sliceinput = new sly_Form_Fragment();
+			$sliceinput->setContent('<div class="sly-contentpage-slice-input">'.$renderer->renderInput('slicevalue').'</div>');
+
+			$bb = new sly_Form_ButtonBar(array(
+				'submit' => new sly_Form_Input_Button('submit', 'btn_save', t('save')),
+				'apply'  => new sly_Form_Input_Button('submit', 'btn_update', t('apply')),
+				'reset'  => new sly_Form_Input_Button('reset', 'reset', t('reset'))
+			));
+
+			$form->add($sliceinput);
+
+			$form->add($bb);
+			$form->addClass('sly-slice-form');
+
+			echo $form->render();
+
 			self::focusFirstElement();
 
 			sly_Core::dispatcher()->notify('SLY_SLICE_POSTVIEW_EDIT', $values, array(
@@ -135,7 +121,7 @@ class sly_Helper_Content {
 
 	private static function focusFirstElement() {
 		$layout = sly_Core::getLayout();
-		$layout->addJavaScript('jQuery(function($) { $("#addslice, #editslice").find(":input:visible:enabled:not([readonly]):first").focus(); });');
+		$layout->addJavaScript('jQuery(function($) { $(".sly-slice-form").find(":input:visible:enabled:not([readonly]):first").focus(); });');
 	}
 
 	public static function metaFormAddButtonBar($form, $label, $name) {
