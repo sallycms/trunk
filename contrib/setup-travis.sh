835382d23ad3f0a139704f8c64923ee7655f5748
Christoph <christoph@webvariants.de>
2012-06-18 04:31:23 +0200

    Backed out changeset: fc127b5e0f55

--- contrib/setup-travis.sh
+++ contrib/setup-travis.sh
@@ -9,7 +9,7 @@ then
 fi
 
 curl -s http://getcomposer.org/installer | php
-php composer.phar install
+php composer.phar install --dev
 
 mysql -e "CREATE DATABASE sally_test"
 mysql --database=sally_test < sally/core/install/mysql.sql
